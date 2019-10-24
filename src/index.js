c91a2ab7b9022d4790a861547ca71263dec2a4f5
thomashanahan <thomas shanahan>
2019-10-24 23:08:58 +0300

    fix issue

--- src/index.js
+++ src/index.js
@@ -22,7 +22,6 @@ const app = (
     </BrowserRouter>
   </Fragment>
 );
-
 ReactDOM.render(app, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
